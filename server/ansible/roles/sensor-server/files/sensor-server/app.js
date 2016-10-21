ba4bf04f20fb1eaff4889e63f8d295c601df0a18
kaija <kaija.chang@gmail.com>
2016-10-21 09:45:55 +0800

    add sentry report

--- server/ansible/roles/sensor-server/files/sensor-server/app.js
+++ server/ansible/roles/sensor-server/files/sensor-server/app.js
@@ -4,15 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var raven = require('raven');
 
 var routes = require('./routes/index');
 var sensors = require('./routes/sensors');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +23,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/error', function mainHandler(req, res) {
+    console.log('error');
+    throw new Error('Broke!');
+});
+
 app.use('/', routes);
 app.use('/sensors', sensors);
+
+app.use(raven.middleware.express.errorHandler(''));
+app.use(raven.middleware.express.requestHandler(''));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

85358849fe6a80fc825fded8cde17bada56d9b3a
kaija <kaija.chang@gmail.com>
2016-10-24 23:49:57 +0800

    update sensor api

--- server/ansible/roles/sensor-server/files/sensor-server/routes/sensors.js
+++ server/ansible/roles/sensor-server/files/sensor-server/routes/sensors.js
@@ -1,12 +1,30 @@
 var express = require('express');
-
-var influx = require('influx');
-client = influx({
-  hosts: ['http://127.0.0.1'],
-  username: 'root',
-  password: 'root',
-  database: 'trend-sensor'
+var os = require('os');
+const database = 'trend-sensor';
+const Influx = require('influx');
+const influx = new Influx.InfluxDB({
+ host: 'localhost',
+ database: database,
+ schema: [
+   {
+     measurement: 'sensors',
+     fields: {
+       temperature: Influx.FieldType.FLOAT,
+       humidity: Influx.FieldType.FLOAT,
+       pm1: Influx.FieldType.FLOAT,
+       pm2_5: Influx.FieldType.FLOAT,
+       pm10: Influx.FieldType.FLOAT,
+       building: Influx.FieldType.STRING,
+       area: Influx.FieldType.STRING,
+       floor: Influx.FieldType.STRING
+     },
+     tags: [
+       'host'
+     ]
+   }
+ ]
 });
+influx.createDatabase(database);
 
 var router = express.Router();
 
@@ -17,7 +35,18 @@ router.get('/all', function(req, res, next) {
 
 router.post('/report', function(req, res, next) {
   console.log(req.body);
-  res.send('OK');
+  body = {"temperature":24, "humidity": 45, "building":"B", "floor": "3", "area":"A", "pm1": 1, "pm2_5": 2, "pm10": 3};
+  influx.writePoints([
+    {
+      measurement: 'sensors',
+      tags: { host: os.hostname() },
+      fields: body
+    }
+  ]).then(()=> {
+    res.send('OK');
+  }).catch((error) => {
+    res.send(error);
+  });
 });
 
 module.exports = router;

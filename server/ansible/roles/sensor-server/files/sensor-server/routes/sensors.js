cf6b3cd25936c6a35edaf6d79c6da918d8ee1eb4
kaija <kaija.chang@gmail.com>
2016-10-25 05:05:27 +0100

    Add install grafana and fixed data format in API

--- server/ansible/roles/sensor-server/files/sensor-server/routes/sensors.js
+++ server/ansible/roles/sensor-server/files/sensor-server/routes/sensors.js
@@ -16,10 +16,11 @@ const influx = new Influx.InfluxDB({
        pm10: Influx.FieldType.FLOAT,
        building: Influx.FieldType.STRING,
        area: Influx.FieldType.STRING,
-       floor: Influx.FieldType.STRING
+       floor: Influx.FieldType.STRING,
+       ip: Influx.FieldType.STRING
      },
      tags: [
-       'host'
+       'mac'
      ]
    }
  ]
@@ -34,19 +35,38 @@ router.get('/all', function(req, res, next) {
 });
 
 router.post('/report', function(req, res, next) {
-  console.log(req.body);
-  body = {"temperature":24, "humidity": 45, "building":"B", "floor": "3", "area":"A", "pm1": 1, "pm2_5": 2, "pm10": 3};
+  //body = {"temperature":24, "humidity": 45, "building":"B", "floor": "3", "area":"A", "pm1": 1, "pm2_5": 2, "pm10": 3, "mac": "b8:27:eb:20:6c:1c", "ip": "10.1.201.195"};
+  req.body;
+  body = {};
+  body['temperature'] = parseFloat(req.body.temperature);
+  body['humidity'] = parseFloat(req.body.humidity);
+  body['pm1'] = parseFloat(req.body.pm1);
+  body['pm2_5'] = parseFloat(req.body.pm2_5);
+  body['pm10'] = parseFloat(req.body.pm10);
+  body['building'] = req.body.building;
+  body['floor'] = req.body.floor;
+  body['area'] = req.body.area;
+  body['ip'] = req.body.ip;
+  mac = req.body.mac;
+  delete body.mac;
+  console.log({
+      measurement: 'sensors',
+      tags: { mac: mac },
+      fields: body
+    });
   influx.writePoints([
     {
       measurement: 'sensors',
-      tags: { host: os.hostname() },
+      tags: { mac: mac },
       fields: body
     }
   ]).then(()=> {
     res.send('OK');
   }).catch((error) => {
+    console.log(error);
     res.send(error);
   });
+
 });
 
 module.exports = router;

dbefc4f36d7c0fe31685ef13751c9c0810312307
kaija <kaija.chang@gmail.com>
2016-10-31 22:01:30 +0800

    refactor pi sensor installer

--- pi/bootstrap.sh
+++ pi/bootstrap.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-apt-get install -y ansible git
+echo "Install Raspberry Pi package"
+apt-get install -y upstart
+pip install requests raven configparser
+echo "Install sensor source code"
+cp -rf pi-sensor /usr/local/
+echo "Install upstart script"
+cp -f pi-sensor.conf /etc/init/
+
+echo "Configure config file"
+cp -f config-pi.py /usr/local/bin/config-pi
+chmod +x /usr/local/bin/config-pi
+/usr/local/bin/config-pi
+
+echo "Reload upstart configuration"
+initctl reload-configuration
+echo "start pi-sensor service"
+service pi-sensor start
---
+++ pi/config-pi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import ConfigParser
+config = ConfigParser.RawConfigParser()
+
+config.add_section('server')
+config.add_section('client')
+url=raw_input('Please input server DNS/IP:( default: sensor.elasticservice.co)')
+if not url:
+    url="sensor.elasticservice.co"
+print "server:" + url
+config.set('server', 'server', url)
+
+building=raw_input('Building:(default: B)')
+if not building:
+    building="B"
+print "building:" + building
+config.set('client', 'building', building)
+
+floor=raw_input('Floor:(default: 3)')
+if not floor:
+    floor="3"
+print "floor:" + floor
+config.set('client', 'floor', floor)
+
+area=raw_input('Area:(default: a)')
+if not area:
+    area="a"
+print "area:" + area
+config.set('client', 'area', area)
+
+with open('/etc/pi-sensor.conf', 'wb') as configfile:
+    config.write(configfile)

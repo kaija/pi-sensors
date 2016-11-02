1a52e08964687f56d0da69cde4325d3a08bff96f
kaija <kaija.chang@gmail.com>
2016-11-02 02:24:57 +0000

    update install script

--- pi/bootstrap.sh
+++ pi/bootstrap.sh
@@ -7,8 +7,8 @@ cp -rf pi-sensor /usr/local/
 cp -rf pi-monitor/pi-monitor /usr/local/bin/
 
 echo "Install systemd script..."
-cp -f pi-sensor.service /etc/systemd/system/
-cp -f pi-monitor/pi-monitor.service /etc/systemd/system/
+cp -f pi-sensor.service /etc/systemd/system/pi_sensor.service
+cp -f pi-monitor/pi-monitor.service /etc/systemd/system/pi_monitor.service
 
 echo "Configure config file..."
 cp -f config-pi.py /usr/local/bin/config-pi
@@ -17,6 +17,8 @@ chmod +x /usr/local/bin/config-pi
 
 echo "Reload systemd configuration..."
 systemctl daemon-reload
+systemctl enable pi_sensor
+systemctl enable pi_monitor
 
 echo "start pi-sensor service..."
 service pi-sensor start
